90ea08557a7ab744582fbedbef88fc3a4ada522a
pldb <pldb.github@gmail.com>
2021-04-07 18:43:06 +0900

    refactor method location

--- lib/zakuro/operation/operation.rb
+++ lib/zakuro/operation/operation.rb
@@ -24,5 +24,22 @@ module Zakuro
     def self.month_histories
       MONTH_HISTORIES
     end
+
+    #
+    # 変更履歴を特定する
+    #
+    # @param [Western::Calendar] western_date 月初日の西暦日
+    #
+    # @return [Operation::MonthHistory] 変更履歴
+    #
+    def self.specify_history(western_date:)
+      month_histroies = Operation.month_histories
+
+      month_histroies.each do |history|
+        return history if western_date == history.western_date
+      end
+
+      Operation::MonthHistory.new
+    end
   end
 end
--- lib/zakuro/version/senmyou/range/operated_range.rb
+++ lib/zakuro/version/senmyou/range/operated_range.rb
@@ -16,8 +16,6 @@ module Zakuro
     class OperatedRange
       # @return [FullRange] 完全範囲
       attr_reader :full_range
-      # @return [Array<MonthHistory>] 変更履歴
-      attr_reader :month_histroies
       # @return [OperatedSolarTerms] 運用時二十四節気
       attr_reader :operated_solar_terms
       #
@@ -27,8 +25,7 @@ module Zakuro
       #
       def initialize(full_range: FullRange.new)
         @full_range = full_range
-        @month_histroies = Operation.month_histories
-        @operated_solar_terms = OperatedSolarTerms.new(full_range: full_range)
+        @operated_solar_terms = OperatedSolarTerms.new(full_range: @full_range)
         @operated_solar_terms.create
       end
 
@@ -67,7 +64,7 @@ module Zakuro
         result = Year.new(multi_gengou: year.multi_gengou, new_year_date: year.new_year_date)
         year.months.each do |month|
           operated_month = month
-          history = specify_history(western_date: month.western_date)
+          history = Operation.specify_history(western_date: month.western_date)
           operated_month = rewrite_month(month: month, history: history) unless history.invalid?
           result.push(month: operated_month)
         end
@@ -75,21 +72,6 @@ module Zakuro
         result.commit
 
         result
-      end
-
-      #
-      # 変更履歴を特定する
-      #
-      # @param [Western::Calendar] western_date 月初日の西暦日
-      #
-      # @return [Operation::MonthHistory] 変更履歴
-      #
-      def specify_history(western_date:)
-        @month_histroies.each do |history|
-          return history if western_date == history.western_date
-        end
-
-        Operation::MonthHistory.new
       end
 
       #
