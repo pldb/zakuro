d75532556451011b4adf3559c84b81c72626bd4e
pldb <pldb.github@gmail.com>
2021-04-09 12:43:23 +0900

    refactor test case for multi data

--- spec/zakuro/version/senmyou/specifier/single_day_specifier_spec.rb
+++ spec/zakuro/version/senmyou/specifier/single_day_specifier_spec.rb
@@ -10,6 +10,8 @@ require File.expand_path('../../../../../' \
 require File.expand_path('./single_day_factory',
                          __dir__)
 
+# rubocop:disable Metrics/BlockLength
+
 describe 'Zakuro' do
   describe 'Senmyou' do
     describe 'Single' do
@@ -24,7 +26,7 @@ describe 'Zakuro' do
             class_prefix: 'Zakuro::Result'
           )
         end
-        it 'all single data' do
+        context 'all single data' do
           filepath = File.expand_path(
             './yaml/testdata.yaml',
             __dir__
@@ -32,19 +34,19 @@ describe 'Zakuro' do
           hash = YAML.load_file(filepath)
 
           hash.each do |test|
-            p test['western_date']
-            p test['test_case_name']
-
             expected = SingleDayFactory.create(hash: test['expected'])
 
-            # TODO: テスト方法
-            eql?(
-              date: Zakuro::Western::Calendar.parse(str: test['western_date']),
-              expected: expected
-            )
+            it "#{test['western_date']}: #{test['test_case_name']}" do
+              eql?(
+                date: Zakuro::Western::Calendar.parse(str: test['western_date']),
+                expected: expected
+              )
+            end
           end
         end
       end
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
