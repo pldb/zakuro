1aa901e353efda4073c5c73e45b94798e9596705
pldb <pldb.github@gmail.com>
2021-04-29 10:17:46 +0900

    refactor code

--- lib/zakuro/version/senmyou/stella/solar_average.rb
+++ lib/zakuro/version/senmyou/stella/solar_average.rb
@@ -11,40 +11,13 @@ module Zakuro
       # @return [Remainder] 気策（24分の1年）
       SOLAR_TERM_AVERAGE = Remainder.new(day: 15, minute: 1835, second: 5)
 
+      #
+      # 初期化
+      #
+      # @param [Integer] western_year 西暦年
+      #
       def initialize(western_year:)
         @solar_term = SolarAverage.first_solar_term(western_year: western_year)
-      end
-
-      def self.first_solar_term(western_year:)
-        # 天正冬至
-        winter_solstice = WinterSolstice.calc(western_year: western_year)
-
-        # 二十四節気（冬至）
-        solar_term = SolarTerm.new(index: 0, remainder: winter_solstice)
-
-        solar_location_index = SolarAverage.calc_solar_location_index(western_year: western_year)
-
-        solar_term.prev_by_index(solar_location_index)
-
-        solar_term
-      end
-
-      def self.calc_solar_location_index(western_year:)
-        # 天正閏余
-        winter_solstice_age = \
-          WinterSolstice.calc_moon_age(western_year: western_year)
-
-        # 入定気を求める
-        first_solar_term = SolarLocation.get(
-          solar_term: SolarTerm.new(remainder: winter_solstice_age)
-        )
-
-        solar_term_index = first_solar_term.index
-
-        # 入定気の一つ後の二十四節気まで戻す（ただし11月経朔が二十四節気上にある場合は戻さない）
-        solar_term_index += 1 unless first_solar_term.remainder == Remainder.new(total: 0)
-
-        solar_term_index
       end
 
       #
@@ -68,6 +41,60 @@ module Zakuro
       end
 
       #
+      # 計算開始する二十四節気を求める
+      #
+      # @param [Integer] western_year 西暦年
+      #
+      # @return [SolarTerm] 二十四節気
+      #
+      def self.first_solar_term(western_year:)
+        # 天正冬至
+        winter_solstice = WinterSolstice.calc(western_year: western_year)
+
+        # 二十四節気（冬至）
+        solar_term = SolarTerm.new(index: 0, remainder: winter_solstice)
+
+        first_solar_term_index = SolarAverage.calc_fist_solar_term_index(western_year: western_year)
+
+        # 対象の二十四節気まで戻す
+        solar_term.prev_by_index(first_solar_term_index)
+
+        solar_term
+      end
+
+      #
+      # 計算開始する二十四節気番号を求める
+      #
+      # * 前提として入定気は冬至の手前にある
+      # * 例えば、定気が大雪であれば入定気は大雪の範囲内にある
+      # * 入定気は、定気の開始位置に重複しない限り、常に定気より後にある
+      # * 基本的に定気の一つ前から起算すれば、当時から求めた11月(閏10/閏11月)に二十四節気を割り当てられる
+      #
+      # @param [Integer] western_year 西暦年
+      #
+      # @return [Integer] 二十四節気番号
+      #
+      def self.calc_fist_solar_term_index(western_year:)
+        # 天正閏余
+        winter_solstice_age = \
+          WinterSolstice.calc_moon_age(western_year: western_year)
+
+        # 入定気を求める
+        solar_location = SolarLocation.get(
+          solar_term: SolarTerm.new(remainder: winter_solstice_age)
+        )
+
+        solar_term_index = solar_location.index
+
+        # 入定気の一つ後の二十四節気まで戻す（ただし11月経朔が二十四節気上にある場合は戻さない）
+        solar_term_index += 1 unless solar_location.remainder == Remainder.new(total: 0)
+
+        solar_term_index
+      end
+
+      # :reek:TooManyStatements { max_statements: 7 }
+
+      #
       # 月内（当月朔日から当月末日（来月朔日の前日）の間）に二十四節気があるか
       # @note 大余60で一巡するため 以下2パターンがある
       #   * current_month <= next_month : (二十四節気) >= current_month && (二十四節気) < next_month
@@ -80,7 +107,7 @@ module Zakuro
       # @return [True] 対象の二十四節気がある
       # @return [False] 対象の二十四節気がない
       #
-      def self.in_range_solar_term?(solar_term:, current_month:, next_month:)
+      def self.in_solar_term?(solar_term:, current_month:, next_month:)
         # 大余で比較する
         target_time = solar_term.day
         current_month_time = current_month.day
@@ -95,35 +122,45 @@ module Zakuro
 
       private
 
+      # :reek:TooManyStatements { max_statements: 8 }
+
+      #
+      # 二十四節気を設定する
+      #
+      # @param [Month] current_month 当月
+      # @param [Month] next_month 次月
+      #
       def set_solar_term(current_month:, next_month:)
         # 安全策として無限ループは回避する
         # * 最大試行回数：4回（設定なし => 設定あり => 設定あり => 設定なし）
         # * 閏月は1回しか設定しない
         # * 最大2回設定する（中気・節気）
         (0..3).each do |_index|
-          in_range = SolarAverage.in_range_solar_term?(
+          in_range = SolarAverage.in_solar_term?(
             solar_term: @solar_term.remainder, current_month: current_month.remainder,
             next_month: next_month.remainder
           )
 
-          if in_range
-            current_month.add_term(term: @solar_term.clone)
+          # 範囲外
+          unless in_range
+            # 1つ以上設定されていれば切り上げる（一つ飛ばしで二十四節気を設定することはない）
+            break unless current_month.empty_solar_term?
+
             next_solar_term
-
-            # 宣明暦は最大2つまで
-            break if current_month.solar_term_size == 2
-
-            # 範囲内であれば続行する
             next
           end
 
-          # 1つ以上設定されていれば切り上げる（一つ飛ばしで二十四節気を設定することはない）
-          break unless current_month.empty_solar_term?
-
+          current_month.add_term(term: @solar_term.clone)
           next_solar_term
+
+          # 宣明暦は最大2つまで
+          break if current_month.solar_term_size == 2
         end
       end
 
+      #
+      # 次の二十四節気に移る
+      #
       def next_solar_term
         @solar_term.next!
       end
