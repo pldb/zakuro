2530abe6c25732c67361e111d6e93de52cc135a0
pldb <pldb.github@gmail.com>
2021-05-06 23:21:28 +0900

    update to comment in senmyou method
    関連メソッドを一時的に復元する

--- lib/zakuro/version/gihou/stella/lunar_orbit.rb
+++ lib/zakuro/version/gihou/stella/lunar_orbit.rb
@@ -92,6 +92,63 @@ module Zakuro
 
       # TODO: 補正値算出
 
+      #
+      # 月の運行による補正値を算出する
+      #
+      # @param [Remainder] remainder_month 月の大余小余
+      #
+      # @return [Integer] 補正値
+      #
+      def self.calc_moon_orbit_value(remainder_month:)
+        valid?(remainder: remainder_month)
+
+        day, minute = make_calculable_remainder_value(remainder: remainder_month)
+
+        # 引き当て
+        adjustment, diff, minute = specify_moon_adjustment(
+          day: day, minute: minute
+        )
+        day = make_cumulative_value_for_days(per: adjustment.per,
+                                             denominator: diff, minute: minute)
+
+        adjustment.stack + day
+      end
+
+      # TODO: 宣明暦のまま
+
+      #
+      # 大余小余を計算可能な値にする
+      # @note 大余の秒（second）は使わない
+      #
+      # @param [LunarRemainder] remainder 大余小余
+      #
+      # @return [Integer] 大余
+      # @return [Integer] 小余
+      #
+      def self.make_calculable_remainder_value(remainder:)
+        day = remainder.day
+        minute = remainder.minute + (remainder.second / 12)
+        minute = minute.floor
+
+        [day, minute]
+      end
+      private_class_method :make_calculable_remainder_value
+
+      #
+      # 大余小余を検証する
+      #
+      # @param [Remainder] remainder 大余小余
+      #
+      # @return [True] 正しい（月の位相計算に使う大余小余）
+      # @return [True] 正しくない
+      #
+      def self.valid?(remainder:)
+        return if remainder.is_a?(Cycle::LunarRemainder)
+
+        raise ArgumentError, "unmatch parameter type: #{remainder.class}"
+      end
+      private_class_method :valid?
+
       # :reek:TooManyStatements { max_statements: 9 }
 
       #
@@ -161,142 +218,34 @@ module Zakuro
       end
       private_class_method :extract_data_from_moon_adjustment_key
 
-      #   #
-      #   # 月の運行による補正値を算出する
-      #   #
-      #   # @param [Remainder] remainder_month 月の大余小余
-      #   # @param [True, False] is_forward 進（遠地点より数える）/退（近地点より数える）
-      #   #
-      #   # @return [Integer] 補正値
-      #   #
-      #   def self.calc_moon_orbit_value(remainder_month:, is_forward:)
-      #     valid?(remainder: remainder_month)
+      # TODO: 宣明暦のまま
 
-      #     day, minute = make_calculable_remainder_value(remainder: remainder_month)
+      # :reek:TooManyStatements { max_statements: 9 }
 
-      #     # 引き当て
-      #     adjustment, diff, minute = specify_moon_adjustment(
-      #       is_forward: is_forward, day: day, minute: minute
-      #     )
-      #     day = make_cumulative_value_for_days(per: adjustment.per,
-      #                                          denominator: diff, minute: minute)
+      #
+      # 累計値（大余）を作成する
+      #
+      # @param [Integer] per 入暦（1-14）
+      # @param [Integer] denominator 小余の分母
+      # @param [Integer] minute 小余
+      #
+      # @return [Integer] 累計値（大余）
+      #
+      def self.make_cumulative_value_for_days(per:, denominator:, minute:)
+        remainder_minute = (per * minute).to_f
+        day = remainder_minute / denominator
+        # 切り捨て（プラスマイナスに関わらず小数点以下切り捨て）
+        day = day.negative? ? day.ceil : day.floor
+        sign = remainder_minute.negative? ? -1 : 1
+        remainder_day = (sign * remainder_minute) % denominator
+        # 四捨五入（8400ならその半分の4200以上を繰り上げる）
+        day += sign if remainder_day >= (denominator / 2)
 
-      #     adjustment.stack + day
-      #   end
+        day
+      end
+      private_class_method :make_cumulative_value_for_days
 
-      #   #
-      #   # 大余小余を検証する
-      #   #
-      #   # @param [Remainder] remainder 大余小余
-      #   #
-      #   # @return [True] 正しい（月の位相計算に使う大余小余）
-      #   # @return [True] 正しくない
-      #   #
-      #   def self.valid?(remainder:)
-      #     return if remainder.is_a?(Cycle::LunarRemainder)
-
-      #     raise ArgumentError, "unmatch parameter type: #{remainder.class}"
-      #   end
-      #   private_class_method :valid?
-
-      #   #
-      #   # 大余小余を計算可能な値にする
-      #   # @note 大余の秒（second）は使わない
-      #   #
-      #   # @param [LunarRemainder] remainder 大余小余
-      #   #
-      #   # @return [Integer] 大余
-      #   # @return [Integer] 小余
-      #   #
-      #   def self.make_calculable_remainder_value(remainder:)
-      #     day = remainder.day
-      #     minute = remainder.minute + (remainder.second / 100)
-      #     minute = minute.floor
-
-      #     [day, minute]
-      #   end
-      #   private_class_method :make_calculable_remainder_value
-
-      #   # :reek:TooManyStatements { max_statements: 9 }
-
-      #   #
-      #   # 累計値（大余）を作成する
-      #   #
-      #   # @param [Integer] per 入暦（1-14）
-      #   # @param [Integer] denominator 小余の分母
-      #   # @param [Integer] minute 小余
-      #   #
-      #   # @return [Integer] 累計値（大余）
-      #   #
-      #   def self.make_cumulative_value_for_days(per:, denominator:, minute:)
-      #     remainder_minute = (per * minute).to_f
-      #     day = remainder_minute / denominator
-      #     # 切り捨て（プラスマイナスに関わらず小数点以下切り捨て）
-      #     day = day.negative? ? day.ceil : day.floor
-      #     sign = remainder_minute.negative? ? -1 : 1
-      #     remainder_day = (sign * remainder_minute) % denominator
-      #     # 四捨五入（8400ならその半分の4200以上を繰り上げる）
-      #     day += sign if remainder_day >= (denominator / 2)
-
-      #     day
-      #   end
-      #   private_class_method :make_cumulative_value_for_days
-
-      #   # :reek:TooManyStatements { max_statements: 9 }
-
-      #   #
-      #   # 月軌道の補正に必要な基本値を引き当てる
-      #   #
-      #   # @note 戻り値は calc_moon_orbit_value で使用する
-      #   #
-      #   # @param [True, False] is_forward 進（遠地点より数える）/退（近地点より数える）
-      #   # @param [Integer] day 大余
-      #   # @param [Integer] minute 小余
-      #   #
-      #   # @return [Adjustment::Item] 補正値
-      #   # @return [Integer] （小余を処理する時の）分母
-      #   # @return [Integer] 小余の下げ幅
-      #   #
-      #   def self.specify_moon_adjustment(is_forward:, day:, minute:)
-      #     prefix = { true => 'forward', false => 'back' }[is_forward]
-
-      #     targets = Adjustment::LIST.select \
-      #       { |key, _| key.match(/^#{prefix}_#{format('%<day>02d', day: day)}_.*/) }
-
-      #     targets.each do |key, value|
-      #       # NOTE: 境界値は上から順に引き当てた方を返す（7日の境界値7465は上のキーで返す）
-      #       matched, diff = \
-      #         extract_data_from_moon_adjustment_key(key, minute)
-      #       # 小余の下げ幅
-      #       calc_minute = (day == 7 && minute > 7465 ? minute - 7465 : minute)
-      #       return value, diff, calc_minute if matched
-      #     end
-
-      #     [nil, nil, nil]
-      #   end
-      #   private_class_method :specify_moon_adjustment
-
-      #   #
-      #   # 補正値を引き当てる
-      #   #
-      #   # @param [String] key 補正値のキー
-      #   # @param [Integer] minute 小余
-      #   #
-      #   # @return [Adjustment::Item] 補正値
-      #   # @return [Integer] （小余を処理する時の）分母
-      #   #
-      #   def self.extract_data_from_moon_adjustment_key(key, minute)
-      #     matched = key.match(/([0-9]{4})_([0-9]{4})$/)
-      #     start = matched[1].to_i
-      #     finish = matched[2].to_i
-
-      #     matched = minute >= start && minute <= finish
-      #     [matched, (finish - start)]
-      #   end
-      #   private_class_method :extract_data_from_moon_adjustment_key
-
-      #   # :reek:ControlParameter and :reek:BooleanParameter
-      #   # :reek:LongParameterList { max_params: 4 }
+      # TODO: 月地点の計算は別モジュールでは？
 
       #   #
       #   # 月地点を計算する
