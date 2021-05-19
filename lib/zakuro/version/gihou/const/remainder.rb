8b9d947257411679acb8f18e5f98c504858f84ef
pldb <pldb.github@gmail.com>
2021-05-19 12:27:23 +0900

    fix gihou const

--- lib/zakuro/version/gihou/const/remainder.rb
+++ lib/zakuro/version/gihou/const/remainder.rb
@@ -20,24 +20,22 @@ module Zakuro
         module Solar
           # @return [Remainder] 気策（24分の1年）
           SOLAR_TERM_AVERAGE = Remainder.new(day: 15, minute: 292, second: 5)
-          # TODO: 宣明暦のまま
-          # @return [Cycle::Remainder] 弦（1分=8秒）
-          QUARTER = Cycle::Remainder.new(day: 7, minute: 3214, second: 2)
+          # TODO: 仮置きする
+          #   3214.25 / 8400 * 1340 = 512.7494047...
+          # @return [Cycle::Remainder] 弦（1分=6秒）
+          QUARTER = Cycle::Remainder.new(day: 7, minute: 512, second: 5)
         end
 
         #
         # Lunar 月
         #
         module Lunar
-          # TODO: 宣明暦のまま
-          # @return [Cycle::LunarRemainder] 暦中日
-          # @note ANOMALISTIC_MONTH の半分に相当する
-          HALF_ANOMALISTIC_MONTH = \
-            Cycle::LunarRemainder.new(day: 13, minute: 6529, second: 9.5)
+          # TODO: Lunar::Adjustment のコメント参照。誤っていた場合は訂正すること
           # @return [Cycle::LunarRemainder] 入暦上限
-          LIMIT = Cycle::LunarRemainder.new(day: 14, minute: 6529, second: 0)
-          # @return [Cycle::LunarRemainder] 弦（1分=100秒）
-          QUARTER = Cycle::LunarRemainder.new(day: 7, minute: 3214, second: 25)
+          LIMIT = Cycle::LunarRemainder.new(day: 28, minute: 743, second: 0)
+          # TODO: 仮置きする
+          # @return [Cycle::LunarRemainder] 弦（1分=12秒）
+          QUARTER = Cycle::LunarRemainder.new(day: 7, minute: 512, second: 10)
         end
       end
     end
