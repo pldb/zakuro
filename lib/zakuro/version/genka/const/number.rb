ecba989e323374ca338d227a47c145388890a12e
pldb <pldb.github@gmail.com>
2021-09-05 00:48:04 +0900

    add genka const module

---
+++ lib/zakuro/version/genka/const/number.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# :nodoc:
+module Zakuro
+  # :nodoc:
+  module Genka
+    #
+    # Const 定数
+    #
+    module Const
+      #
+      # Number 数値
+      #
+      module Number
+        #
+        # Cycle 周期
+        #
+        module Cycle
+          # @return [Integer] 日法（1日=752分）
+          DAY = 752
+          # @return [Integer] 朔望月
+          SYNODIC_MONTH = 22_207
+        end
+
+        #
+        # 累積
+        #
+        module Stack
+          # @return [Integer] 積年（甲子夜半朔旦冬至〜暦の開始前）
+          TOTAL_YEAR = 5703
+          # @return [Integer] 暦の開始年（元嘉20年）
+          BEGIN_YEAR = 443
+        end
+      end
+    end
+  end
+end
