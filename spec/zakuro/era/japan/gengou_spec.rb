7c597432134def824f1b76b6c3d32eb64bd9017e
pldb <pldb.github@gmail.com>
2021-10-26 23:22:20 +0900

    update gengou test cases

--- spec/zakuro/era/japan/gengou_spec.rb
+++ spec/zakuro/era/japan/gengou_spec.rb
@@ -55,10 +55,19 @@ describe 'Zakuro' do
               expect(actual.name).to eq('test-001')
             end
           end
-          context 'end_date' do
+          context 'western date in both_end_date' do
             it 'is the same data in file' do
               expect(actual.both_end_date.western).to eq(
                 Zakuro::Western::Calendar.new(year: 555, month: 2, day: 3)
+              )
+            end
+          end
+          context 'japan date in both_end_date' do
+            it 'is the same data in file' do
+              expect(actual.both_end_date.japan.format).to eq(
+                Zakuro::Japan::Calendar.new(
+                  gengou: '元号', year: 2, leaped: false, month: 1, day: 10
+                ).format
               )
             end
           end
@@ -80,7 +89,16 @@ describe 'Zakuro' do
                 expect(item.name).to eq('元号名1')
               end
             end
-            context 'start_date' do
+            context 'japan date in both_start_date' do
+              it 'is the same data in file' do
+                expect(item.both_start_date.japan.format).to eq(
+                  Zakuro::Japan::Calendar.new(
+                    gengou: '元号', year: 2, leaped: false, month: 1, day: 11
+                  ).format
+                )
+              end
+            end
+            context 'westen date in both_start_date' do
               it 'is the same data in file' do
                 expect(item.both_start_date.western).to eq(
                   Zakuro::Western::Calendar.new(year: 501, month: 1, day: 11)
@@ -94,9 +112,14 @@ describe 'Zakuro' do
                 )
               end
             end
-            context 'year' do
+            context 'japan year in both_start_year' do
               it 'is the same data in file' do
                 expect(item.both_start_year.japan).to eq(11)
+              end
+            end
+            context 'western year in both_start_year' do
+              it 'is the same data in file' do
+                expect(item.both_start_year.western).to eq(1001)
               end
             end
           end
@@ -107,7 +130,16 @@ describe 'Zakuro' do
                 expect(item.name).to eq('元号名3')
               end
             end
-            context 'start_date' do
+            context 'japan date in both_start_date' do
+              it 'is the same data in file' do
+                expect(item.both_start_date.japan.format).to eq(
+                  Zakuro::Japan::Calendar.new(
+                    gengou: '元号', year: 2, leaped: false, month: 11, day: 13
+                  ).format
+                )
+              end
+            end
+            context 'westen date in both_start_date' do
               it 'is the same data in file' do
                 expect(item.both_start_date.western).to eq(
                   Zakuro::Western::Calendar.new(year: 501, month: 12, day: 13)
@@ -121,9 +153,14 @@ describe 'Zakuro' do
                 )
               end
             end
-            context 'year' do
+            context 'japan year in both_start_year' do
               it 'is the same data in file' do
                 expect(item.both_start_year.japan).to eq(13)
+              end
+            end
+            context 'western year in both_start_year' do
+              it 'is the same data in file' do
+                expect(item.both_start_year.western).to eq(1003)
               end
             end
           end
