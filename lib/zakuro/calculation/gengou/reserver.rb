1ac34042da06e801bbf742e03a7acb54844dafc3
pldb <pldb.github@gmail.com>
2021-11-01 08:21:23 +0900

    create gengou reserver module

---
+++ lib/zakuro/calculation/gengou/reserver.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../../era/western/calendar'
+
+# :nodoc:
+module Zakuro
+  # :nodoc:
+  module Calculation
+    # :nodoc:
+    module Gengou
+      # Reserver
+      #
+      # 元号に基づき計算範囲を計算する
+      #
+      module Reserver
+        def self.get(start: Western::Calendar.new, end: Western::Calendar.new)
+          # TODO: todo
+        end
+      end
+    end
+  end
+end
