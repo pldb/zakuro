79b41558af7965d490d3a829e31fdb497529b41e
pldb <pldb.github@gmail.com>
2021-11-01 12:25:39 +0900

    create reserved interval

--- lib/zakuro/calculation/gengou/reserver.rb
+++ lib/zakuro/calculation/gengou/reserver.rb
@@ -10,10 +10,18 @@ module Zakuro
     module Gengou
       # Reserver
       #
-      # 元号に基づき計算範囲を計算する
+      # 元号に基づき計算範囲を予約する
       #
       module Reserver
-        def self.get(start: Western::Calendar.new, end: Western::Calendar.new)
+        #
+        # 予約結果を取得する
+        #
+        # @param [Western::Calendar] start_date 開始日
+        # @param [Western::Calendar] end_date 終了日
+        #
+        # @return [ReservedInterval] 予約済み計算範囲
+        #
+        def self.get(start_date: Western::Calendar.new, end_date: Western::Calendar.new)
           # TODO: todo
         end
       end
