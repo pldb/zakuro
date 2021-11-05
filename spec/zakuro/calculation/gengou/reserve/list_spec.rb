222175ac1cde979178da1f859c682a0b4ef2c13b
pldb <pldb.github@gmail.com>
2021-11-05 12:26:00 +0900

    update to add test cases on reserve list

--- spec/zakuro/calculation/gengou/reserve/list_spec.rb
+++ spec/zakuro/calculation/gengou/reserve/list_spec.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../../../../lib/zakuro/calculation/gengou/reserve/l
 require File.expand_path('../../../../../lib/zakuro/era/western/calendar',
                          __dir__)
 
+# rubocop:disable Metrics/BlockLength
 describe 'Zakuro' do
   describe 'Calculation' do
     describe 'Gengou' do
@@ -21,6 +22,27 @@ describe 'Zakuro' do
               actual = list.get
               expect(actual.size).to eq 1
             end
+            it 'should be target gengou' do
+              list = Zakuro::Calculation::Gengou::Reserve::List.new(
+                method_name: :first_line,
+                start_date: Zakuro::Western::Calendar.new(year: 450, month: 1, day: 2),
+                end_date: Zakuro::Western::Calendar.new(year: 450, month: 1, day: 2)
+              )
+              actual = list.get
+              expect(actual[0].name).to eq '允恭天皇'
+            end
+          end
+          context 'western date has two gengou' do
+            it 'should be two elements in result array' do
+              list = Zakuro::Calculation::Gengou::Reserve::List.new(
+                method_name: :first_line,
+                start_date: Zakuro::Western::Calendar.new(year: 454, month: 2, day: 13),
+                end_date: Zakuro::Western::Calendar.new(year: 454, month: 2, day: 14)
+              )
+              actual = list.get
+              # TODO: fix test
+              expect(actual.size).to eq 2
+            end
           end
           # TODO: more tests
         end
@@ -28,3 +50,4 @@ describe 'Zakuro' do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
