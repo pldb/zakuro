6b5db3a20489098faaf684c44dd9562b1b110db3
pldb <pldb.github@gmail.com>
2022-01-15 18:36:03 +0900

    update to add 'start_year' each calendar

--- lib/zakuro/era/japan/reki.rb
+++ lib/zakuro/era/japan/reki.rb
@@ -21,55 +21,77 @@ module Zakuro
         attr_reader :class_name
         # @return [Western::Calendar] 暦の開始日
         attr_reader :start_date
+        # @return [Integer] 開始西暦年
+        attr_reader :start_year
 
         #
         # 初期化
         #
         # @param [String] class_name 暦のクラス名
         # @param [Western::Calendar] start_date 暦の開始日
+        # @param [Integer] start_year 開始西暦年
         #
-        def initialize(class_name:, start_date:)
+        def initialize(class_name:, start_date:, start_year:)
           @class_name = class_name
           @start_date = start_date
+          @start_year = start_year
         end
       end
 
       LIST = [
+        # 允恭天皇34年01月01日
         Range.new(
           class_name: 'Zakuro::Genka::Gateway',
-          start_date: Western::Calendar.new(year: 445, month: 1, day: 24)
+          start_date: Western::Calendar.new(year: 445, month: 1, day: 24),
+          start_year: 445
         ),
+        # 文武天皇02年01月01日
         Range.new(
           class_name: 'Zakuro::Gihou::Gateway',
-          start_date: Western::Calendar.new(year: 698, month: 2, day: 16)
+          start_date: Western::Calendar.new(year: 698, month: 2, day: 16),
+          start_year: 698
         ),
+        # 天平宝字08年01月01日
         Range.new(
           class_name: 'Zakuro::Daien::Gateway',
-          start_date: Western::Calendar.new(year: 764, month: 2, day: 7)
+          start_date: Western::Calendar.new(year: 764, month: 2, day: 7),
+          start_year: 764
         ),
+        # 貞観02年01月01日
         Range.new(
           class_name: 'Zakuro::Senmyou::Gateway',
-          start_date: Western::Calendar.new(year: 862, month: 2, day: 3)
+          start_date: Western::Calendar.new(year: 862, month: 2, day: 3),
+          start_year: 862
         ),
+        # 貞享02年01月01日
         Range.new(
           class_name: 'Zakuro::Joukyou::Gateway',
-          start_date: Western::Calendar.new(year: 1685, month: 2, day: 4)
+          start_date: Western::Calendar.new(year: 1685, month: 2, day: 4),
+          start_year: 1685
         ),
+        # 宝暦05年01月01日
         Range.new(
           class_name: 'Zakuro::Houryaku::Gateway',
-          start_date: Western::Calendar.new(year: 1755, month: 2, day: 11)
+          start_date: Western::Calendar.new(year: 1755, month: 2, day: 11),
+          start_year: 1755
         ),
+        # 寛政10年01月01日
         Range.new(
           class_name: 'Zakuro::Kansei::Gateway',
-          start_date: Western::Calendar.new(year: 1798, month: 2, day: 16)
+          start_date: Western::Calendar.new(year: 1798, month: 2, day: 16),
+          start_year: 1798
         ),
+        # 天保13年01月01日
         Range.new(
           class_name: 'Zakuro::Tenpou::Gateway',
-          start_date: Western::Calendar.new(year: 1844, month: 2, day: 18)
+          start_date: Western::Calendar.new(year: 1844, month: 2, day: 18),
+          start_year: 1844
         ),
+        # 明治01年09月08日
         Range.new(
           class_name: 'Zakuro::Gregorio::Gateway',
-          start_date: Western::Calendar.new(year: 1872, month: 12, day: 9)
+          start_date: Western::Calendar.new(year: 1868, month: 10, day: 23),
+          start_year: 1868
         )
       ].freeze
 
