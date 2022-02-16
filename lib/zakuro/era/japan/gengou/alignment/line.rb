516e1bf7c11e0988c035854d34c61f2bdcb38988
pldb <pldb.github@gmail.com>
2022-02-16 22:36:22 +0900

    refactor line class

--- lib/zakuro/era/japan/gengou/alignment/line.rb
+++ lib/zakuro/era/japan/gengou/alignment/line.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative './division'
+
 require_relative './linear_gengou'
 
 # :nodoc:
@@ -12,9 +14,6 @@ module Zakuro
       # Line 行
       #
       class Line
-        # TODO: refactor
-        # TODO: test
-
         # @return [Array<LinearGengou>] 元号
         attr_reader :list
 
@@ -77,7 +76,7 @@ module Zakuro
             result |= and!(rest: list, other: gengou)
           end
 
-          connect(list: result)
+          Division.connect(list: result)
         end
 
         #
@@ -91,7 +90,7 @@ module Zakuro
             surplus_result = not!(surplus: surplus_result, other: gengou)
           end
 
-          surplus_result = connect(list: surplus_result)
+          surplus_result = Division.connect(list: surplus_result)
 
           surplus_result.each do |gengou|
             @list.push(gengou)
@@ -109,7 +108,7 @@ module Zakuro
         def and!(rest:, other:)
           result = []
           rest.each do |gengou|
-            result |= duplicate_gengou(this: gengou, other: other)
+            result |= Division.match(this: gengou, other: other)
           end
 
           result
@@ -126,138 +125,7 @@ module Zakuro
         def not!(surplus:, other:)
           result = []
           surplus.each do |gengou|
-            result |= sub_gengou(this: gengou, other: other)
-          end
-
-          result
-        end
-
-        #
-        # 重複した範囲を返す
-        #
-        # @param [LinearGengou] this 残り元号
-        # @param [LinearGengou] other 比較元号
-        #
-        # @return [Array<LinearGengou>] 重複分
-        #
-        def duplicate_gengou(this:, other:)
-          result = []
-          start_date = this.start_date
-          last_date = this.last_date
-          if other.in?(start_date: start_date, last_date: last_date)
-            result.push(this)
-            return result
-          end
-          if other.out?(start_date: start_date, last_date: last_date)
-            # 該当なし
-            return result
-          end
-
-          if other.covered?(start_date: start_date, last_date: last_date)
-            # 範囲内のみ返す
-            result.push(
-              LinearGengou.new(
-                start_date: other.start_date, last_date: other.last_date,
-                gengou: this.gengou
-              )
-            )
-            return result
-          end
-
-          start = start_date > other.start_date ? start_date : other.start_date
-          last = last_date < other.last_date ? last_date : other.last_date
-
-          result.push(
-            LinearGengou.new(
-              start_date: start.clone, last_date: last.clone, gengou: this.gengou
-            )
-          )
-          result
-        end
-
-        #
-        # 範囲が重複しない差分を返す
-        #
-        # @param [LinearGengou] this 積元号
-        # @param [LinearGengou] other 比較元号
-        #
-        # @return [Array<LinearGengou>] 差分
-        #
-        def sub_gengou(this:, other:)
-          result = []
-          start_date = this.start_date
-          last_date = this.last_date
-          if other.in?(start_date: start_date, last_date: last_date)
-            # 該当なし
-            return result
-          end
-
-          if other.out?(start_date: start_date, last_date: last_date)
-            result.push(this)
-            return result
-          end
-          if other.covered?(start_date: start_date, last_date: last_date)
-            result.push(
-              LinearGengou.new(
-                start_date: start_date.clone, last_date: other.start_date.clone - 1,
-                gengou: this.gengou
-              ),
-              LinearGengou.new(
-                start_date: other.last_date.clone + 1, last_date: last_date.clone,
-                gengou: this.gengou
-              )
-            )
-            return result
-          end
-
-          start = start_date
-          last = last_date
-
-          # 開始日が比較元号の開始日より前の範囲
-          last = other.start_date.clone - 1 if start_date < other.start_date
-
-          # 終了日が比較元号の開始日より後の範囲
-          start = other.last_date.clone + 1 if last_date > other.last_date
-
-          result.push(
-            LinearGengou.new(
-              start_date: start.clone, last_date: last.clone, gengou: this.gengou
-            )
-          )
-          result
-        end
-
-        #
-        # 比較により分離した元号をつなげる
-        #
-        # @param [Array<LinearGengou>] list 元号
-        #
-        # @return [Array<LinearGengou>] 同一元号がつながった元号
-        #
-        def connect(list: [])
-          result = []
-          list.each do |linear_gengou|
-            if result.size.zero?
-              result.push(linear_gengou)
-              next
-            end
-
-            before = result[-1]
-
-            unless linear_gengou.gengou.name == before.gengou.name
-              result.push(linear_gengou)
-              next
-            end
-
-            unless (before.last_date.clone + 1) == linear_gengou.start_date
-              result.push(linear_gengou)
-              next
-            end
-
-            result[-1] = LinearGengou.new(
-              start_date: before.start_date, last_date: linear_gengou.last_date,
-              gengou: linear_gengou.gengou
-            )
+            result |= Division.mismatch(this: gengou, other: other)
           end
 
           result
