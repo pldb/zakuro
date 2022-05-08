b0a195a98ced3db24230038379142ba896731474
pldb <pldb.github@gmail.com>
2022-05-08 22:00:54 +0900

    update to create remainder for motsunichi

--- lib/zakuro/version/senmyou/cycle/remainder.rb
+++ lib/zakuro/version/senmyou/cycle/remainder.rb
@@ -76,6 +76,40 @@ module Zakuro
 
           super(form, @day, @minute, @second)
         end
+
+        #
+        # LunarRemainder 没日の計算向け時刻情報（没余）
+        #
+        class MotsunichiRemainder < Calculation::Cycle::AbstractRemainder
+          # @return [Integer] 通余
+          MINUTE = Const::Number::Derivation::REMAINDER_ALL_YEAR
+
+          #
+          # 初期化
+          #
+          # @param [Integer] day 大余（"日"に相当）
+          # @param [Integer] minute 小余（"分"に相当）
+          # @param [Integer] second 秒
+          # @param [Integer] total 繰り上げなしの小余
+          #
+          def initialize(day: -1, minute: -1, second: -1, total: -1)
+            super(base_day: Const::Number::Cycle::DAY, base_minute: MINUTE,
+                  day: day, minute: minute, second: second, total: total)
+          end
+
+          #
+          # 特定の文字フォーマットにして出力する
+          #
+          # @param [String] form フォーマット（大余、小余、秒それぞれを%dで指定する）
+          #
+          # @return [String] フォーマットした結果
+          #
+          def format(form: '%d-%d')
+            return '' if invalid?
+
+            super(form, @day, @minute, @second)
+          end
+        end
       end
     end
   end
