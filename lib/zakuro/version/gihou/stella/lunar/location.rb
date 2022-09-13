1eadb4892a56ebf52868182825983d26d08d357f
pldb <pldb.github@gmail.com>
2022-09-13 12:47:40 +0900

    refactor to remove useless methods

--- lib/zakuro/version/gihou/stella/lunar/location.rb
+++ lib/zakuro/version/gihou/stella/lunar/location.rb
@@ -28,7 +28,11 @@ module Zakuro
         # @param [Integer] western_year 西暦年
         #
         def initialize(lunar_age:, western_year:)
-          super(lunar_age: lunar_age, western_year: western_year)
+          super(
+            lunar_age: lunar_age,
+            quarter: QUARTER,
+            western_year: western_year
+          )
         end
 
         #
@@ -41,13 +45,6 @@ module Zakuro
           end
 
           first
-        end
-
-        #
-        # 弦の分だけ月地点を進める
-        #
-        def add_quarter
-          remainder.add!(QUARTER)
         end
 
         private
