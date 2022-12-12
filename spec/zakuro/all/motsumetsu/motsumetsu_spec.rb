bf44fb9b6f27219b37e81cae60d69b22acb28a90
pldb <pldb.github@gmail.com>
2022-12-12 22:08:25 +0900

    create motsunichi empty test cases

---
+++ spec/zakuro/all/motsumetsu/motsumetsu_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative './abstract/parser'
+
+describe 'Zakuro' do
+  describe 'All' do
+    describe 'Motsumetsu' do
+      context 'xxx' do
+        it 'yyy' do
+          # TODO: use
+          data = Zakuro::All::Motsumetsu::Parser.get
+          p data.size
+        end
+      end
+    end
+  end
+end
