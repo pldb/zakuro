4b66641dbe23365a2ef58653f7ea34e15e1ceac2
pldb <pldb.github@gmail.com>
2022-12-13 22:36:33 +0900

    fix to collect every dates

--- spec/zakuro/all/motsumetsu/abstract/parser.rb
+++ spec/zakuro/all/motsumetsu/abstract/parser.rb
@@ -13,12 +13,14 @@ module Zakuro
       # テストデータを解析する
       #
       module Parser
+        FIRST_LINE_REGEX = /^([一-龥]{2,4}) ([0-9]{1,2})年\(([0-9]{3,4})\)・・・(.+)/.freeze
+
         # TODO: refactor
         class << self
           #
           # データを取得する
           #
-          # @return [<Type>] テストデータ
+          # @return [Array<Year>] テストデータ
           #
           def get
             result = []
@@ -26,13 +28,14 @@ module Zakuro
 
             File.open(fullpath, 'r') do |f|
               f.each_line do |line|
-                text = line.gsub("\n", '')
+                text = line.strip
 
                 next if text == ''
 
-                year = first_line(line: text)
-
-                next if year.invalid?
+                if first_line?(line: line)
+                  year = first_line(line: text)
+                  next
+                end
 
                 second_line(year: year, line: text)
 
@@ -49,40 +52,44 @@ module Zakuro
             Reference.path
           end
 
+          def first_line?(line:)
+            line.match?(FIRST_LINE_REGEX)
+          end
+
           def first_line(line:)
-            m = line.match(/^([一-龥]{2,4}) ([0-9]{1,2})年\(([0-9]{3,4})\)・・・(.+)/)
+            m = line.match(FIRST_LINE_REGEX)
 
             return Year.new unless m
 
-            japan_dates = []
             gengou = Gengou.new(name: m[1], year: m[2].to_i, western_year: m[3].to_i)
-            dates = m[4].split(',')
-            dates.each do |date|
-              next if date == ''
 
-              parsed = JapanDate.new(text: date)
+            dates = japan_dates(line: m[4])
 
-              japan_dates.push(parsed)
-            end
-
-            Year.new(gengou: gengou, dates: japan_dates)
+            Year.new(gengou: gengou, dates: dates)
           end
 
           def second_line(year:, line:)
-            m = line.match(/^\s+(.+)/)
+            dates = year.dates
 
-            return unless m
+            result = japan_dates(line: line)
 
-            japan_dates = year.dates
+            dates.concat(result)
+          end
 
-            dates = m[1].split(',')
+          def japan_dates(line:)
+            result = []
+            dates = line.split(',')
             dates.each do |date|
+              date = date.strip
+
               next if date == ''
 
               parsed = JapanDate.new(text: date)
 
-              japan_dates.push(parsed)
+              result.push(parsed)
             end
+
+            result
           end
         end
       end
