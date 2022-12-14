948e74958d2c29af8dd152db0b7f02f37ae17dcf
pldb <pldb.github@gmail.com>
2022-12-14 12:28:46 +0900

    update a test case for motsunichi and metsunichi

--- spec/zakuro/all/motsumetsu/motsumetsu_spec.rb
+++ spec/zakuro/all/motsumetsu/motsumetsu_spec.rb
@@ -5,11 +5,18 @@ require_relative './abstract/parser'
 describe 'Zakuro' do
   describe 'All' do
     describe 'Motsumetsu' do
-      context 'xxx' do
-        it 'yyy' do
-          # TODO: use
-          data = Zakuro::All::Motsumetsu::Parser.get
-          p data.size
+      context 'all motsunichi and metsunichi' do
+        it 'should be equal to a reference' do
+          years = Zakuro::All::Motsumetsu::Parser.get
+          years.each do |year|
+            gengou = year.gengou
+            year.dates.each do |date|
+              # TODO: use
+              p "#{gengou.name}#{gengou.year}年#{date.leaped ? '閏' : ''}#{date.month}月#{date.day}日"
+              p "没日: #{date.dropped}"
+              p "滅日: #{date.vanished}"
+            end
+          end
         end
       end
     end
