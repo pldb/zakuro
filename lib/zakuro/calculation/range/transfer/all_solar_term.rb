2fe117b78ee247883b2564cde9dba7baea5f9761
pldb <pldb.github@gmail.com>
2023-01-10 23:10:02 +0900

    update to use all solar term module

--- lib/zakuro/calculation/range/transfer/all_solar_term.rb
+++ lib/zakuro/calculation/range/transfer/all_solar_term.rb
@@ -23,7 +23,9 @@ module Zakuro
             # @param [Array<Array<Month>>] annual_ranges 年データ（冬至基準）
             #
             def update_months(annual_ranges:)
-              # TODO: make
+              annual_ranges.each do |months|
+                collect(months: months)
+              end
             end
 
             #
@@ -32,7 +34,35 @@ module Zakuro
             # @param [Array<Base::Year>] annual_ranges 年データ（元旦基準）
             #
             def update_years(annual_ranges:)
-              # TODO: make
+              annual_ranges.each do |year|
+                collect(months: year.months)
+              end
+            end
+
+            private
+
+            #
+            # 二十四節気を収集する
+            #
+            # @param [Array<Month>] months 月情報
+            #
+            def collect(months:)
+              # TODO: refactor
+              months.each_with_index do |month, index|
+                next if index.zero?
+
+                all_solar_terms = []
+                all_solar_terms.push(months[index - 1].solar_terms[-1].clone)
+                all_solar_terms.concat(month.solar_terms)
+
+                months[index] = Monthly::InitializedMonth.new(
+                  context: context,
+                  month_label: month.month_label, first_day: month.first_day,
+                  solar_terms: month.solar_terms, phase_index: month.phase_index,
+                  is_last_year: month.is_last_year,
+                  meta: Meta.new(all_solar_terms: all_solar_terms)
+                )
+              end
             end
           end
         end
